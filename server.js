c07ffdaa6d57a5a18515ef56b2ec1dac4b5e0b30
bitsoko <bitsoko@bitsoko.io>
2016-10-19 13:17:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ if (url.pathname.substring(0, 2) == '/p' && url.hostname == location.hostname) {
 		//resolve(Response.redirect('http://bitsoko.io/bits/#m='+bid));
 //orReq.url='https://bitsoko.io/p'+bid+'?f=j';
 
-  var respo = cache.match(event.request.clone()).then(function (response) {
+  return cache.match(event.request.clone()).then(function (response) {
 	  
 console.log(response);
- var resp = response || fetch('https://bitsoko.io/p'+bid+'?f=j').then(function(response) {
+ return response || fetch('https://bitsoko.io/p'+bid+'?f=j').then(function(response) {
 //response=Response.redirect('https://bitsoko.io/p='+bid+'?format=json');
 
 //console.log(response);
@@ -37,7 +37,17 @@ if(response.clone().url=='https://bitsoko.io/p'+bid+'?f=j' && event.request.clon
 	
 cache.put(event.request.clone(), response.clone());
 
-return response;	
+return response.clone().text().then(function(d){
+ 	
+var respJ = JSON.parse(d);
+  console.log(respJ);
+url = location.origin+'/bits/index.html#s='+respJ.s+'&a='+respJ.a;
+
+return Response.redirect(url);
+
+
+ 	
+ });	
 	
 }
 
@@ -50,28 +60,9 @@ return Response.redirect('https://bitsoko.io/bits/index.html#s=3&a=404');
   });	
 //cache.put(orReq, response.clone());
 //  console.log(resp.clone());
-return resp;
- 
+
   });
-	console.log(respo);
- var respon;
-	respon = respo.text().then(function(d){
- 	
-var respJ = JSON.parse(d);
-  console.log(respJ);
-//var resp=JSON.parse(resp);
-  //  var blob = new Blob([JSON.stringify(json)], { type: 'application/json' });
 
-   // return new Response(blob, { headers: response.headers });
-
-url = location.origin+'/bits/index.html#s='+respJ.s+'&a='+respJ.a;
-
-return Response.redirect(url);
-
-
- 	
- });
- return respon;
 }
 		
  }
