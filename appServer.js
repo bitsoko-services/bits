7fee5aa00ea7fc0a24b8ac447f5b973d8ef06964
bitsoko <bitsoko@bitsoko.io>
2017-05-21 13:31:41 +0300

    Update appServer.js

--- appServer.js
+++ appServer.js
@@ -4,5 +4,43 @@ exports.incomingOrder = function(req,res) {
     
    console.log('AI bot request!! ',req.body);
 	
+    try {
+        var speech = 'empty speech';
+
+        if (req.body) {
+            var requestBody = req.body;
+
+            if (requestBody.result) {
+                speech = '';
+
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
+
+                if (requestBody.result.action) {
+                    speech += 'action: ' + requestBody.result.action;
+                }
+            }
+        }
+
+        console.log('result: ', speech);
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'apiai-webhook-sample'
+        });
+    } catch (err) {
+        console.error("Can't process request", err);
+
+        return res.status(400).json({
+            status: {
+                code: 400,
+                errorType: err.message
+            }
+        });
+    }
+	
         
   };
