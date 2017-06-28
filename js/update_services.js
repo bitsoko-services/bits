aa87b6a119a4a553ffba3ffb7c59c76da9dcd78c
sambitsoko <samuel@bitsoko.io>
2017-06-28 23:53:05 +0300

    update 454

--- js/update_services.js
+++ js/update_services.js
@@ -38,30 +38,7 @@ servicePageLoader();
 	 subscribedPromos();
 	 } 
 ///////////////////////////////////------------------- OID check -----------------------------------------//////////////////////////////////////////////////
-if (window.location.hash != undefined ) 
- {	 
-//check if hash is oid
-var type = window.location.hash.substr(1);
-console.log(type)
-// split the hash
-var fields = type.split('=');
-var htpe = fields[0];
-var hval = fields[1];
-console.log( htpe )
-console.log( hval)
-if(htpe == "oid"){console.log("its an order")
-//get the shop and the oder details
-var shop = getBitsWinOpt('s')
-// oid
-console.log("this is shop id "+ shop +" the oid is "+ hval)
-$('#oderModal').openModal();
-}
-else{console.log("we dont know this hash")}
-  
-         }
-else{
-         
-	 }	 
+oid(); 
 ///.////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if (getBitsWinOpt('o') != undefined ) {
 doFetch({ action: 'storesBO',  id:parseInt(getBitsWinOpt('o'))  }).then(
