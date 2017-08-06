79d520a59a1500eb1756f0deb0970e628e546ada
bitsoko <bitsoko@bitsoko.io>
2017-08-06 08:56:45 +0300

    Update bits_contacts.js

--- js/bits_contacts.js
+++ js/bits_contacts.js
@@ -14,17 +14,45 @@ function contact(){
          document.querySelector('.cardLogo').src = "https://bitsoko.io/app/images/services/contacts.png";
 		document.querySelector('.serviceDescription').innerHTML = "All your contacts";
     var allContacts = JSON.parse(event.target.result);
-  	for(var iii = 0 ;  iii < allContacts.length; ++iii) { 
-		console.log("contacts")	 
-			//var dailyCost=(parseInt(mDet.promotions[ii].discount)/100)*mDet.promotions[ii].promoPrice;
+	var contPage = {};	
+	var contPageUser = {};	
+	contPageUser.payments='true';
+		contPageUser.theme="#147930";
+		
+  	for(var iii = 0, contPage=contPage, contPageUser=contPageUser;  iii < allContacts.length; ++iii) { 
+		console.log("contacts");
+			
 			var id = allContacts[iii].uid ? allContacts[iii].uid : 'undefined';
 			if(allContacts[iii].img.includes('https://')){
 		    var final_url=allContacts[iii].img
 			//$('#contactsImg').removeClass("bits-dis-image");
+		    }else{ 
+			    var final_url='https://bitsoko.io/'+allContacts[iii].img;
 		    }
-		    else{ var final_url='https://bitsoko.io/'+allContacts[iii].img;}
-		 $('.serviceListHolder').append('<li onclick="selectContact('+id+')" class="collection-item waves-effect avatar" style="background-color: inherit; width: 100%;"><span class="title"><span class="serviceListTitle" style="margin-left: 20px;"> '+allContacts[iii].name+' </span><img src="'+final_url+'" alt="" id="contactsImg" class="circle bits-dis-image"><span class="title"><span class="serviceListTitle"></span></span><p class="serviceListFirstline">'+allContacts[iii].contact+' <br class="servicelistSeccondline"> </li>');   
+		
+		if(parseInt(getBitsWinOpt('a'))==parseInt(allContacts[iii].uid)){
+		contPageUser.id=parseInt(allContacts[iii].uid);
+		contPageUser.bannerPath=parseInt(allContacts[iii].img);
+		contPageUser.name=parseInt(allContacts[iii].name);
+			
+		}
+		
+		// $('.serviceListHolder').append('<li onclick="selectContact('+id+')" class="collection-item waves-effect avatar" style="background-color: inherit; width: 100%;"><span class="title"><span class="serviceListTitle" style="margin-left: 20px;"> '+allContacts[iii].name+' </span><img src="'+final_url+'" alt="" id="contactsImg" class="circle bits-dis-image"><span class="title"><span class="serviceListTitle"></span></span><p class="serviceListFirstline">'+allContacts[iii].contact+' <br class="servicelistSeccondline"> </li>');   
             } 
+		contPage.list=allContacts;
+		contPageUser.list=[];
+		
+	if(getBitsWinOpt('a')==undefined){
+		
+	populateService(contPage)	
+	}else{
+	
+	populateService(contPageUser)
+	}	
+		
+		
+		
+	
 	}
 catch(e) {  	console.log("no contacts")	 
        $('.serviceListHolder').append('<li onclick="reqContacts()" class="collection-item waves-effect avatar"  style="background-color: inherit; width: 100%;"><img src="" alt="" class="circle bits-dis-image"><span class="title"><span class="serviceListTitle">Empty</span></span><p class="serviceListFirstline"> Click to update your  list <br class="servicelistSeccondline">  </p><a href="#!" class="secondary-content"></a></li>');
