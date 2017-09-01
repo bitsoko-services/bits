b84670da3a911e24e8620e9a33ad769be724ee59
bitsoko <bitsoko@bitsoko.io>
2017-09-01 21:42:22 +0300

    Update bitsCore.js

--- js/bitsCore.js
+++ js/bitsCore.js
@@ -253,8 +253,7 @@ function loadWallet(primWalA){
 	
     data = primWalA;
      showAddr('bitcoin:'+data);
-   	$('.addressClass').append('0x'+localStorage.getItem('bits-user-address-'+ localStorage.getItem('bits-user-name')));
-
+   	
         var stor=getObjectStore('data', 'readwrite').get('user-profile-'+localStorage.getItem('bits-user-name'));
 	stor.onsuccess = function (event) {
   try{
@@ -283,15 +282,8 @@ function loadWallet(primWalA){
                  getBal();
                  //updateWallpaper();
             //setWallpaper();
-             
-      doFetch({ action: 'getServs', user: localStorage.getItem('bits-user-name') }).then(function(e){
-          if(e.data.length==0){
-              
-      doFetch({ action: 'noServices', loc: localStorage.getItem('bits-settings-country')});
-          }
-              
-              
-      });
+	$('.addressClass').append('0x'+localStorage.getItem('bits-user-address-'+ localStorage.getItem('bits-user-name')));
+
     });
     updateBal();
     	getLoc();
