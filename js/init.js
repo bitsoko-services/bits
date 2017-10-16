8306adfd672cf9c2fee5db1db22b5fe969a07e13
bitsoko <bitsoko@bitsoko.io>
2017-10-16 18:16:25 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -77,10 +77,6 @@ function starting(){
 if(checkanon()){
 	
 	walletFunctions(localStorage.getItem('bits-user-name')).then(function(e){
-	
-	});
-	 		
-	
 				/////////////////////////////////// update exchange rates
 	 fetchRates().then(function(e) {
 		if (e.status == "ok") {
@@ -92,6 +88,10 @@ if(checkanon()){
 		}
 	});
 	///////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	});
+	 		
+	
 	
 	
 	
