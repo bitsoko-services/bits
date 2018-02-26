2c393d118464c0e5a4910a65e37191b8d8a6cdea
gaseema-n <gaseema@bitsoko.io>
2018-02-26 17:30:23 +0300

    activate notifications update

--- js/functions.js
+++ js/functions.js
@@ -371,6 +371,7 @@ function makeOrder(orderArrayy, orderLoc) {
                         },
                         service: parseInt(getBitsWinOpt('s'))
                     }).then(function (e) {
+                        $("#appendPushSubs").remove();
                         if (e.status == "ok") {
                             console.log('5');
 
@@ -378,6 +379,7 @@ function makeOrder(orderArrayy, orderLoc) {
 
                             $('#modalconfirm').closeModal();
                             swal("success!", "your order has been sent!", "success");
+                            $(".sweet-alert .sa-button-container").prepend('<div id="appendPushSubs"><div class="switch"> <span class="js-push-button-notification-title bits-13" style="">Activate notifications to track your order</span> <label><input class="js-push-button-notification" style="background: rgb(128, 210, 147);" type="checkbox"> <span class="lever right" style=" margin-top: 4px; margin-right: 5%;"></span></label> </div><br></div>')
                             clearCart();
                         } else {
                             swal("Cancelled", "your order is not sent", "error");
