4ad3da2c64f9b48272bd88903adae036277b50af
gaseema-n <gaseema@bitsoko.io>
2018-05-08 10:12:01 +0300

    turn on notifications update

--- js/functions.js
+++ js/functions.js
@@ -482,9 +482,15 @@ function makeOrder(orderArrayy, orderLoc) {
                                             if (e.status == "ok") {
                                                 $('#modalconfirm').modal('close');
                                                 //swal("success!", "your order has been sent!", "success");
+                                                var toastHTML = '<span>Turn on notifications</span><button class="btn-flat toast-action" onclick="startmessage()">Activate</button>';
                                                 M.toast({
-                                                    html: 'Your order has been sent!'
-                                                })
+                                                    html: 'Your order has been sent!',
+                                                    completeCallback: setTimeout(function () {
+                                                        M.toast({
+                                                            html: toastHTML
+                                                        })
+                                                    }, 4000)
+                                                });
                                                 $(".sweet-alert .sa-button-container").prepend('<div id="appendPushSubs"><div class="switch"> <span class="js-push-button-notification-title bits-13" style="">Activate notifications to track your order</span> <label><input onclick="startPushManager();" class="js-push-button-notification" style="background: rgb(128, 210, 147);" type="checkbox"> <span class="lever right" style=" margin-top: 4px; margin-right: 5%;"></span></label> </div><br></div>')
                                                 clearCart();
                                             } else {
@@ -545,9 +551,16 @@ function makeOrder(orderArrayy, orderLoc) {
                                 if (e.status == "ok") {
                                     $('#modalconfirm').modal("close");
                                     //swal("success!", "your order has been sent!", "success");
+                                    var toastHTML = '<span>Turn on notifications</span><button class="btn-flat toast-action" onclick="startmessage()">Activate</button>';
                                     M.toast({
-                                        html: 'Your order has been sent!'
-                                    })
+                                        html: 'Your order has been sent!',
+                                        completeCallback: setTimeout(function () {
+                                            M.toast({
+                                                html: toastHTML
+                                            })
+                                        }, 4000)
+                                    });
+
                                     $(".sweet-alert .sa-button-container").prepend('<div id="appendPushSubs"><div class="switch"> <span class="js-push-button-notification-title bits-13" style="">Activate notifications to track your order</span> <label><input class="js-push-button-notification" style="background: rgb(128, 210, 147);" type="checkbox" onclick="startmessage()"> <span class="lever right" style=" margin-top: 4px; margin-right: 5%;"></span></label> </div><br></div>')
                                     clearCart();
                                 } else {
