1e468ad7bfd6ede519eaf1b37fe08307e8fb109c
gaseema-n <gaseema@bitsoko.io>
2018-06-26 12:22:12 +0300

    update 656

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // BITS Server-JavaScript Document
 try {
-    bitsVersion = 655;
+    bitsVersion = 656;
     // 	importScripts('https://bitsoko.co.ke/bitsAssets/js/lightwallet/lightwallet.min.js');
     // 	importScripts('https://bitsoko.co.ke/bitsAssets/js/web3/web3.js');
     // 	importScripts('https://bitsoko.co.ke/bitsAssets/js/hooked-web3-provider/build/hooked-web3-provider.js');
