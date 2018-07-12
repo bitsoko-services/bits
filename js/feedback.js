7893c6d3ce392f34115d8e013527ae527708f785
gaseema-n <gaseema@bitsoko.io>
2018-07-12 12:15:10 +0300

    feedback feature added

--- js/feedback.js
+++ js/feedback.js
@@ -1,3 +1,36 @@
+var functionRun = false;
+
 function feedback() {
-    $("#feedbackModal").modal("open")
+    $("#feedbackModal").modal("open");
+    if (functionRun != true) {
+        functionRun = true;
+        var productQty;
+        var serviceQty;
+        var message;
+        $(".prdQty input").on("change", function (e) {
+            productQty = $(this).attr("rate")
+        })
+        $(".servQty input").on("change", function (e) {
+            serviceQty = $(this).attr("rate")
+        })
+        $("#sendFeedback").click(function (e) {
+            M.toast({
+                html: 'Sending feedback...'
+            })
+            doFetch({
+                action: 'getDeliveryMembers',
+                uid: localStorage.getItem('bits-user-name'),
+                speed: productQty,
+                quality: serviceQty,
+                text: $("#feedbackText").val()
+            }).then(function (e) {
+                if (e.status == "ok") {
+                    M.toast({
+                        html: 'Feed back sent successfully'
+                    })
+                    $("#feedbackModal").modal("close")
+                }
+            })
+        })
+    }
 }
