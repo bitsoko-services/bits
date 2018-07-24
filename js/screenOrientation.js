f20b30293f696f00ae43c243d32d422c28361080
gaseema-n <gaseema@bitsoko.io>
2018-07-24 12:24:00 +0300

    screen orientation fixed to portrait

---
+++ js/screenOrientation.js
@@ -0,0 +1,39 @@
+function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+    } else {
+        cancelFullScreen.call(doc);
+    }
+}
+
+//Prompt user to use app on fullscreen mode
+function fullScreenMode() {
+    if (localStorage.getItem("fullScreenPermission") == null) {
+        $("#fullScreenPermission").modal("open");
+    } else {
+        if (localStorage.getItem("fullScreenPermission") == "true") {
+            $("#fullscreenState").prop("checked", true)
+
+            toggleFullScreen();
+            setTimeout(function () {
+                screen.orientation.lock('portrait')
+            }, 1000)
+        } else {
+            $("#fullscreenState").prop("checked", "false")
+        }
+    }
+}
+document.getElementById("fullscreenState").addEventListener("change", function () {
+    if ($("#fullscreenState")[0].checked == true) {
+        localStorage.setItem("fullScreenPermission", "true");
+        fullScreenMode();
+    } else {
+        localStorage.setItem("fullScreenPermission", "false");
+    }
+})
