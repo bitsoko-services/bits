bb84f5c9793ec2d2a845a42401e36e25680d0df6
bitsoko <bitsoko@bitsoko.io>
2018-08-18 23:16:46 +0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1092,10 +1092,7 @@ $('#modalconfirm').modal('close');
             //                                                        $(".sweet-alert .sa-button-container").prepend('<div id="appendPushSubs"><div class="switch"> <span class="js-push-button-notification-title bits-13" style="">Activate notifications to track your order</span> <label><input onclick="startPushManager();" class="js-push-button-notification" style="background: rgb(128, 210, 147);" type="checkbox"> <span class="lever right" style=" margin-top: 4px; margin-right: 5%;"></span></label> </div><br></div>')
             clearCart();
 
-                                })
-                                .catch((error) => console.log('Error sharing:', error));
-
-                            
+                               
                             ev.preventDefault();
                         } else {
                             console.log("WEBSHARE, this feature is not supported on your browser");
@@ -1120,6 +1117,13 @@ $('#modalconfirm').modal('close');
             M.toast({
                 html: 'Your wishlist has been sent!'
             });
+                                 })
+                                .catch((error) => console.log('Error sharing:', error));
+
+                             })
+                                .catch((error) => console.log('Error sharing:', error));
+
+                            
 }
 
 async function getWishId(){
