f66a1bd3b727ffb1be57272e7b5b44a0fd25909c
bitsoko <bitsoko@bitsoko.io>
2018-08-18 23:59:19 +0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -26,15 +26,7 @@ async function doMakeOrder(orderArrayy, res, globalDel, locOrigin, uid, addrr, p
         if (e.status == "ok") {
             $('#modalconfirm').modal('close');
             //swal("success!", "your order has been sent!", "success");
-            //                                                        var toastHTML = '<span>Turn on notifications</span><button class="btn-flat toast-action" onclick="startmessage()">Activate</button>';
-            M.toast({
-                html: 'Your order has been sent!',
-                completeCallback: setTimeout(function () {
-                    M.toast({
-                        html: toastHTML
-                    })
-                }, 4000)
-            });
+                 
             //                                                        $(".sweet-alert .sa-button-container").prepend('<div id="appendPushSubs"><div class="switch"> <span class="js-push-button-notification-title bits-13" style="">Activate notifications to track your order</span> <label><input onclick="startPushManager();" class="js-push-button-notification" style="background: rgb(128, 210, 147);" type="checkbox"> <span class="lever right" style=" margin-top: 4px; margin-right: 5%;"></span></label> </div><br></div>')
             clearCart();
             return e;
@@ -1102,7 +1094,7 @@ console.log(e);
     // Share it!
                             navigator.share({
                                     title: document.title,
-                                    url: '/bits/?s='+getBitsWinOpt('s')+'&wish='+e.oid
+                                    url: '/bits/?s='+getBitsWinOpt('s')+'&wish='+e
                                 }).then(function (e) {
 return e;
                                  })
@@ -1118,8 +1110,16 @@ async function getWishId(){
                                 "coin": "bits",
                                 "purchase": ''
                             }, parseInt(getBitsWinOpt('s')));
-     
-    var e=await wishShare(e);
+                                                        var toastHTML = '<span>Your wishlist is ready to send</span><button class="btn-flat toast-action" onclick="wishShare('+e.oid+')">Share</button>';
+            M.toast({
+                html: 'Your order has been sent!',
+                completeCallback: setTimeout(function () {
+                    M.toast({
+                        html: toastHTML
+                    })
+                }, 4000)
+            });
+    var e=await wishShare(e.oid);
      
      return e;
                             
